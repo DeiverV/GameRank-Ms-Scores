e286f3448af25359baab3404b1edd7da4904d44d
DeiberV <deiberveranodev@gmail.com>
2024-11-06 19:02:27 -0500

    feat: moongose setup

--- src/scores/scores.module.ts
+++ src/scores/scores.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ScoresController } from './scores.controller';
 import { ScoresService } from './scores.service';
-// import { MongooseModule } from '@nestjs/mongoose';
-// import { Score, ScoreSchema } from './schema';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Score, ScoreSchema } from './schema';
 
 @Module({
-  // imports: [
-  //   MongooseModule.forFeature([{ name: Score.name, schema: ScoreSchema }]),
-  // ],
+  imports: [
+    MongooseModule.forFeature([{ name: Score.name, schema: ScoreSchema }]),
+  ],
   controllers: [ScoresController],
   providers: [ScoresService],
 })
